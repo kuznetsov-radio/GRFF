148cfbfaf4ff0ee2e653d02fb25f557096d7a42e
kuznetsov-radio <51828521+kuznetsov-radio@users.noreply.github.com>
2023-06-12 21:01:47 +0800

    Rename Zeta.h to source/Zeta.h

---
+++ source/Zeta.h
@@ -0,0 +1,3 @@
+#pragma once
+double Zeta_Solar(double T, double f, int ABcode);
+double Zeta_arbitrary(double T, double f, int ABcode, int N_fZ, int N_TZ, double *lnfZ_arr, double *lnTZ_arr, double *Z_arr);
