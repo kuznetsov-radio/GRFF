273daee6ff903cc1b642ff7b97dfb38b11e9c4b5
kuznetsov-radio <51828521+kuznetsov-radio@users.noreply.github.com>
2024-03-06 15:11:03 +0800

    Delete source/IDLinterface.h

--- source/IDLinterface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define D3(s1, s2, i1, i2, i3) ((i1)+((i2)+(i3)*(s2))*(s1))
-
-#ifndef LINUX
-extern "C" __declspec(dllexport) int GET_MW(int argc, void **argv);
-#else
-extern "C" int GET_MW(int argc, void **argv);
-#endif
